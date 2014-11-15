b3917bfab18807bb062d8cad1f4b6a7a0effc6be
Robert Cook <r.cook@oxidecomputing.com>
2014-11-15 13:44:44 +0000

    Revert "Don't force SSLv3"
    This reverts commit eda4b5c9188f3ba4d96d7055ebc23faec41bd767.

--- src/LogicalGrape/PayPalIpnLaravel/PayPalIpn.php
+++ src/LogicalGrape/PayPalIpnLaravel/PayPalIpn.php
@@ -36,7 +36,6 @@ class PayPalIpn
     public function getOrder()
     {
         $request = $this->getRequestHandler();
-        $request->forceSSLv3(FALSE);
 
         $listener = new PayPalListener($request);
         $listener->setMode($this->getEnvironment());
